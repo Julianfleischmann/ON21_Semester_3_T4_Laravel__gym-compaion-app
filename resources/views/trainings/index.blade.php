6806d99d45038ae9cc0fcdf5dcf31c60de7f2730
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-11-21 22:34:12 +0100

    Added Bootstrap to trainings.index

--- resources/views/trainings/index.blade.php
+++ resources/views/trainings/index.blade.php
@@ -5,44 +5,39 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <table class="table">
-                        <tr>
-                            <th>Trainingsart</th>
-                            <th>Trainingsgewicht</th>
-                            <th>Wiederholungen</th>
-                            <th>Erstellt von</th>
-                            <th>Bearbeiten</th>
-                            <th>Löschen</th>
-                        </tr>
+    <div class="container">
+        <div class="bg-white rounded p-4 m-5">
+            <table class="table">
+                <tr>
+                    <th>Trainingsart</th>
+                    <th>Trainingsgewicht</th>
+                    <th>Wiederholungen</th>
+                    <th>Erstellt von</th>
+                    <th>Bearbeiten</th>
+                    <th>Löschen</th>
+                </tr>
 
-                        @foreach ($trainings as $training)
+                @foreach ($trainings as $training)
 
-                            <tr>
-                                <td>{{ $training->getTrainingName->name }}</td>
-                                <td>{{ $training->weight }}</td>
-                                <td>{{ $training->repetition }}</td>
-                                <td>{{ $training->getUser->name }}</td>
-                                <td><a class="btn btn-secondary" href="{{ route('trainings.edit',$training->id) }}">bearbeiten</a></td>
-                                <td>
-                                    <form action="{{ route('trainings.destroy', $training->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-warning" type="submit">löschen</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-{{--                        {{ $trainings }}--}}
-                    </table>
-
-
-                    <a class="btn btn-primary" href="{{ route('trainings.create') }}">Neues Training anlegen</a>
-                </div>
-            </div>
+                    <tr>
+                        <td>{{ $training->getTrainingName->name }}</td>
+                        <td>{{ $training->weight }}</td>
+                        <td>{{ $training->repetition }}</td>
+                        <td>{{ $training->getUser->name }}</td>
+                        <td><a class="btn btn-secondary" href="{{ route('trainings.edit',$training->id) }}">bearbeiten</a></td>
+                        <td>
+                            <form action="{{ route('trainings.destroy', $training->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-warning" type="submit">löschen</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                {{--                        {{ $trainings }}--}}
+            </table>
+            <a class="btn btn-primary" href="{{ route('trainings.create') }}">Neues Training anlegen</a>
         </div>
     </div>
+
 </x-app-layout>
