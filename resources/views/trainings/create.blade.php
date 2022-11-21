4754dae7f41949b985f5689396f17eebfd515f51
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-11-21 12:31:00 +0100

    Fixed dataType in Ajax

--- resources/views/trainings/create.blade.php
+++ resources/views/trainings/create.blade.php
@@ -52,16 +52,35 @@
                 url: "{{ route('training-names.store') }}",
                 // contentType: "application/json; charset=utf-8",
                 data: formData,
-                dataType: "json",
-                success: function (msg) {
-                    alert("This recipe has been saved in your profile area!");
+                // dataType: "json",
+                success: function (data) {
+                    // alert("This recipe has been saved in your profile area!");
+                    console.info(data);
+                    // refreshNames();
                 },
-                // error: function (data) {
-                //     console.log('Error:', data);
-                //     console.info('Ajax name ist: ', data)
-                //     console.info('Feld ist: ', formData)
-                // }
+                error: function (data) {
+                    console.log('Error:', data);
+                    console.info('Ajax name ist: ', data)
+                    console.info('Feld ist: ', formData)
+                }
             });
         });
-    });
+
+    {{--    function refreshNames() {--}}
+    {{--        $.get("{{ URL::to('create') }}", function(data){--}}
+    {{--            $('#createNewTrainingName'.empty().html(data));--}}
+    {{--        })--}}
+    {{--    }--}}
+    {{--    function refreshNames() {--}}
+    {{--        // const o = new Option("text", "123")--}}
+    {{--        // $(o).html("asdfsdffdsffddffddd");--}}
+    {{--        // $("#name").append(o);--}}
+    {{--        $.get("{{ url::to('create') }}", function(){--}}
+    {{--            let o = new Option("otion text", "value")--}}
+    {{--            $(o).html("option text");--}}
+    {{--            $("name").append(o);--}}
+    {{--            // alert("click");--}}
+    {{--        })--}}
+    {{--    }--}}
+    {{--});--}}
 </script>
