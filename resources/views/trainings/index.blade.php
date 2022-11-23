23ad5fdb058c251db22cf921f635f8483a77d8d3
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-11-23 22:59:45 +0100

    Minor fix for button class

--- resources/views/trainings/index.blade.php
+++ resources/views/trainings/index.blade.php
@@ -46,8 +46,8 @@
             </table>
                 {{ $trainings->links() }}
             <a class="btn btn-primary" href="{{ route('trainings.create') }}">Neues Training anlegen</a>
-            <a id="count_trainings" class="btn btn-primary">Wie viele Trains habe ich gemacht?</a>
-            <p>Anzahl der Trainings: <span id="counted_trainings"></span></p>
+            <a id="count_trainings" class="btn btn-secondary">Wie viele Trains habe ich gemacht?</a>
+            <h4 class="mt-4 h4">Anzahl der Trainings: <span id="counted_trainings"></span></h4>
         </div>
     </div>
 
